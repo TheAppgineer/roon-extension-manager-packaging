4cf72437d34deb3f5dc2ad422c75f851d3f691d3
Jan Koudijs <theappgineer@gmail.com>
2020-05-16 11:28:14 +0200

    Implement graceful shutdown of Docker container

--- linux/roon-extension-manager.sh
+++ linux/roon-extension-manager.sh
@@ -1,10 +1,39 @@
-#!/bin/sh
+#!/bin/bash
+
+# Background info: http://veithen.io/2014/11/16/sigterm-propagation.html
+
+# Setup signal handlers
+_term() {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child"
+}
+
+_int() {
+  echo "Caught SIGINT signal!"
+  kill -INT "$child"
+}
+
+trap _term SIGTERM
+trap _int  SIGINT
+
+# Start Roon Extension Manager
 cd node_modules/roon-extension-manager
-node . $1
+node . $1 &
+
+# Wait for termination
+child=$!
+wait "$child"       # Extra wait in case released by a trap
+wait "$child"
 
 if [ $? -eq 66 ]; then
+    # Handle update request
     cd ../roon-extension-manager-updater
-    node .
+    node . &
+
+    # Wait for termination
+    child=$!
+    wait "$child"   # Extra wait in case released by a trap
+    wait "$child"
 fi
 
 cd ../..
